2300b54c4cc20bfaeec6478852f0402658f330d7
AMR <amr@home.local>
2019-06-08 10:51:33 +0200

    Add DrinksRepository

---
+++ DrinkStore/Data/Interfaces/IDrinksRepo.cs
@@ -0,0 +1,15 @@
+﻿using DrinkStore.Data.Models;
+using System.Collections.Generic;
+
+namespace DrinkStore.Data.Repositories
+{
+    public interface IDrinksRepo
+    {
+        IEnumerable<Drink> ListDrinks { get; }
+        IEnumerable<Drink> ListPreferredDrinks { get; }
+        IEnumerable<Drink> GetByCategories(string cat);
+        Drink GetById(int id);
+
+    }
+
+}
---
+++ DrinkStore/Data/Repositories/DrinksRepo.cs
@@ -0,0 +1,50 @@
+﻿using DrinkStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStore.Data.Repositories
+{
+    public class DrinksRepo : IDrinksRepo
+    {
+        private readonly DrinksDbContext db;
+
+        public DrinksRepo(DrinksDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<Drink> ListDrinks => db.drinks.ToList();
+        public IEnumerable<Drink> ListPreferredDrinks => db.drinks.Where(p => p.IsPreferredDrink);
+
+        public IEnumerable<Drink> GetByCategories(string cat)
+        {
+            if (!string.IsNullOrEmpty(cat) || !string.IsNullOrWhiteSpace(cat))
+            {
+                return db.drinks.Include(c => c.Category).Where(s => s.Category.CategoryName == cat);
+            }
+            else
+            {
+                return db.drinks.ToList();
+            }
+        }
+
+        public Drink GetById(int id)
+        {
+            var drink = db.drinks.Include(c => c.Category).SingleOrDefault(s => s.DrinkId == id);
+            if (drink != null)
+            {
+                return drink;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+     
+
+}
+}
