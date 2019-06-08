8a9452cca430065cc394c9c89d551c06d73d6067
AMR <amr@home.local>
2019-06-08 09:21:07 +0200

    migrate database

---
+++ DrinkStore/Data/DrinksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DrinkStore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkStore.Data
+{
+    public class DrinksDbContext:DbContext
+    {
+        public DrinksDbContext(DbContextOptions<DrinksDbContext> options):base(options)
+        {
+        }
+        public DbSet<Drink> drinks { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+    }
+}
---
+++ DrinkStore/Migrations/20190608071824_InitMigration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DrinkStore.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "drinks",
+                columns: table => new
+                {
+                    DrinkId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ShortDescription = table.Column<string>(nullable: true),
+                    LongDescription = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    ImageThumbnailUrl = table.Column<string>(nullable: true),
+                    IsPreferredDrink = table.Column<bool>(nullable: false),
+                    InStock = table.Column<bool>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_drinks", x => x.DrinkId);
+                    table.ForeignKey(
+                        name: "FK_drinks_categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_drinks_CategoryId",
+                table: "drinks",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "drinks");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
